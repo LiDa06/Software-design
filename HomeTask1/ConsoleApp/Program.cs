0032a5eaa63fd17340fc9e5911e0c7302778c312
Dasha Lishmanova <dasha.lishmanova@gmail.com>
2025-10-12 16:54:53 +0300

    Rewrite programm.cs to use DI

--- HomeTask1/ConsoleApp/Program.cs
+++ HomeTask1/ConsoleApp/Program.cs
@@ -1,40 +1,24 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ConsoleApp.Interfaces;
-using ConsoleApp.Alive;
-using ConsoleApp.Inventory;
 using ConsoleApp.Services;
-using System.Collections;
 
 namespace ConsoleApp
 {
-    public class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        static void Main()
         {
-            ServiceCollection services = [];
+            ServiceCollection services = new();
 
-            services.AddTransient<IVetClinic, VetClinic>();
+            services.AddTransient<IConsole, SystemConsole>();
+            services.AddTransient<Communication>();
             services.AddTransient<Zoo>();
+            services.AddTransient<Commands>();
 
             ServiceProvider provider = services.BuildServiceProvider();
 
-            Zoo zoo = provider.GetRequiredService<Zoo>();
-
-            Console.WriteLine("Добро пожаловать в приложение!");
-            bool stopPoint = true;
-            while (stopPoint)
-            {
-                int command = Communication.MainMenue();
-                switch (command)
-                {
-                    case 1: Commands.Command1(ref zoo); break;
-                    case 2: Commands.Command2(ref zoo); break;
-                    case 3: zoo.TotalAmountOfFood(); break;
-                    case 4: zoo.PettingZooAnimals(); break;
-                    case 5: zoo.Inventory(); break;
-                    default: stopPoint = false; break;
-                }
-            }
+            var commands = provider.GetRequiredService<Commands>();
+            commands.Run();
         }
     }
 }
